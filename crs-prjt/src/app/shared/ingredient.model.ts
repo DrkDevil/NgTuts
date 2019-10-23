b5a5f69d3d5e5285eae02b62b5e67cfb2faf2554
Brian Orr <myemail@brianorr.me>
2019-10-23 00:25:32 -0500

    working stuff

--- crs-prjt/src/app/shared/ingredient.model.ts
+++ crs-prjt/src/app/shared/ingredient.model.ts
@@ -1,7 +1,7 @@
 export class ingredient {
   // public name: string;
   // public amount: string;
-  constructor(public name: string, public amount: string ) {
+  constructor(public name: string, public amount: number ) {
     // this.name = name;
     // this.amount = amount;
   }
